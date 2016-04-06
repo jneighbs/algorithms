a01116ae170ed3a85e31927707ca1c22ecd39004
jneighbs <jneighbs88@gmail.com>
2016-04-06 14:35:42 -0500

    starting selection folder, working on linear time selection, cleaned up sort.h a little

--- include/sort.h
+++ include/sort.h
@@ -7,9 +7,12 @@
 #ifndef SORT_H_
 #define SORT_H_
 
+// TODO: implement
+// void countingSort();
+// void bubbleSort(int unsortedArray[], int arrayLength);
+
 void insertionSort(int unsortedArray[], int arrayLength);
 void mergeSort(int unsortedArray[], int arrayLength);
-//void bubbleSort(int unsortedArray[], int arrayLength); TODO: implement
 void selectionSort(int unsortedArray[], int arrayLength);
 void quickSort(int unsortedArray[], int arrayLength);
 
