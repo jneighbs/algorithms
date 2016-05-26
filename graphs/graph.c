7c951c938cb623246beeb1beb2671bf09a3c2ae0
jneighbs <jneighbs88@gmail.com>
2016-05-26 15:17:25 -0500

    added printGraphs fn, though I havent tested it yet

--- graphs/graph.c
+++ graphs/graph.c
@@ -12,3 +12,26 @@ Graph* createGraph(FILE *inputFile)
   // TODO: implement!
   return NULL;
 }
+
+
+// TODO - need to test this
+void printGraph(Graph *g)
+{
+
+  for(int i = 0; i < g->numVertices; i++){
+    Vertex *vp = g->vertices+i;
+    printf("Vertex %d: ", vp->label);
+    ConnectorElement *curElement = vp->head;
+    while(curElement != NULL){
+      int v2Label;
+      if(curElement->adjacentEdge->endpoint1 == curElement){
+        v2Label = curElement->adjacentEdge->endpoint2->sourceVertex->label;
+      } else {
+        v2Label = curElement->adjacentEdge->endpoint1->sourceVertex->label;
+      }
+      printf("(%d,%d) ", vp->label, v2Label);
+      curElement = curElement->next;
+    }
+    printf("\n");
+  }
+}
